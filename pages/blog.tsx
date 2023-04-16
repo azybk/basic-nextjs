127da863cdbf16fcf0eec8c090012ee6374e1b21
azybk <kumbaraazy@gmail.com>
2023-04-16 18:34:22 +0700

    commit

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,41 @@
 import Layout from '../components/Layout';
+import Styles from '../styles/Blog.module.css'
 
-export default function Blog() {
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+
+interface BlogProps {
+    dataBlog: Post[]
+}
+
+export default function Blog(props: BlogProps) {
+    const { dataBlog } = props
+
     return (
         <Layout pageTitle="Blog Page">
-            <p className='title'>Blog Page</p>
+            {dataBlog.map(blog => {
+                return (
+                    <div key={blog.id} className={Styles.card}>
+                        <h3>{blog.title}</h3>
+                        <p>{blog.body}</p>
+                    </div>
+                )
+            })}
         </Layout>
     )
+}
+
+
+export async function getServerSideProps() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const dataBlog = await res.json();
+
+    return {
+        props: {
+            dataBlog
+        }
+    }
 }
